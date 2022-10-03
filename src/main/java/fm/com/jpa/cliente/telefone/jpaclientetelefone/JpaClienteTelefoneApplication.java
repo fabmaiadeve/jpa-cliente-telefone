a5c82b783537c6e9c526e558e2d80f4c80ef3b23
Fabiano Maia <fmaiati@gmail.com>
2022-10-03 09:31:16 -0300

    commit inicial do proj marco zero

---
+++ src/main/java/fm/com/jpa/cliente/telefone/jpaclientetelefone/JpaClienteTelefoneApplication.java
@@ -0,0 +1,13 @@
+package fm.com.jpa.cliente.telefone.jpaclientetelefone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaClienteTelefoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaClienteTelefoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fm/com/jpa/cliente/telefone/jpaclientetelefone/JpaClienteTelefoneApplicationTests.java
@@ -0,0 +1,13 @@
+package fm.com.jpa.cliente.telefone.jpaclientetelefone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaClienteTelefoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
