839bcb3337a4af1ec86e4451165bc4a8499338e6
Fabiano Maia <fmaiati@gmail.com>
2022-10-03 11:09:34 -0300

    Configurando banco H2 e implementando entidades

--- src/main/java/fm/com/jpa/cliente/telefone/jpaclientetelefone/entities/Cliente.java
+++ src/main/java/fm/com/jpa/cliente/telefone/jpaclientetelefone/entities/Cliente.java
@@ -1,4 +1,66 @@
 package fm.com.jpa.cliente.telefone.jpaclientetelefone.entities;
 
-public class Cliente {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_cliente")
+public class Cliente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String login;
+
+    private String senha;
+
+    private String nome;
+
+    @OneToMany(mappedBy = "cliente", orphanRemoval = true, cascade = CascadeType.ALL)
+    private List<Telefone> telefones = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Telefone> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(List<Telefone> telefones) {
+        this.telefones = telefones;
+    }
 }
--- src/main/java/fm/com/jpa/cliente/telefone/jpaclientetelefone/entities/Telefone.java
+++ src/main/java/fm/com/jpa/cliente/telefone/jpaclientetelefone/entities/Telefone.java
@@ -1,4 +1,45 @@
 package fm.com.jpa.cliente.telefone.jpaclientetelefone.entities;
 
-public class Telefone {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_telefone")
+public class Telefone  implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String numero;
+
+    @org.hibernate.annotations.ForeignKey(name = "usuario_id")
+    @ManyToOne
+    private Cliente cliente;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
 }
